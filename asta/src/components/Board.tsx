681a83a9ae40a15016260b35c1d406f809fb7fa4
R0su <2019-104574@rtu.edu.ph>
2023-08-08 14:12:24 +0800

    Update Board.tsx

--- asta/src/components/Board.tsx
+++ asta/src/components/Board.tsx
@@ -9,36 +9,52 @@ function Board() {
 
   const handleDragEnd = (result: DropResult) => {
     const { destination, source, type } = result;
-
+  
     if (!destination) return;
-
+  
     if (type === 'column') {
+      // Reorder columns when dragged within the board
       const newColumns = [...board.columns];
       const [removed] = newColumns.splice(source.index, 1);
       newColumns.splice(destination.index, 0, removed);
       setBoardState({ columns: newColumns });
     } else if (type === 'task') {
-      const sourceColumn = board.columns.find((col) => col.id === source.droppableId);
-      const destinationColumn = board.columns.find((col) => col.id === destination.droppableId);
-
-      if (sourceColumn && destinationColumn) {
-        const sourceTasks = [...sourceColumn.tasks];
-        const [removed] = sourceTasks.splice(source.index, 1);
-        const destinationTasks = [...destinationColumn.tasks];
-        destinationTasks.splice(destination.index, 0, removed);
-
-        const newColumns = board.columns.map((col) =>
-          col.id === source.droppableId
-            ? { ...col, tasks: sourceTasks }
-            : col.id === destination.droppableId
-            ? { ...col, tasks: destinationTasks }
-            : col
-        );
-
-        setBoardState({ columns: newColumns });
+      if (source.droppableId === destination.droppableId) {
+        // Reorder tasks within the same column
+        const column = board.columns.find((col) => col.id === source.droppableId);
+        if (column) {
+          const newTasks = [...column.tasks];
+          const [removed] = newTasks.splice(source.index, 1);
+          newTasks.splice(destination.index, 0, removed);
+  
+          const newColumns = board.columns.map((col) =>
+            col.id === source.droppableId ? { ...col, tasks: newTasks } : col
+          );
+  
+          setBoardState({ columns: newColumns });
+        }
+      } else {
+        // Move task from source to destination column
+        const sourceColumn = board.columns.find((col) => col.id === source.droppableId);
+        const destinationColumn = board.columns.find((col) => col.id === destination.droppableId);
+  
+        if (sourceColumn && destinationColumn) {
+          const sourceTasks = [...sourceColumn.tasks];
+          const destinationTasks = [...destinationColumn.tasks];
+          const [movedTask] = sourceTasks.splice(source.index, 1);
+          destinationTasks.splice(destination.index, 0, movedTask);
+  
+          const newColumns = board.columns.map((col) =>
+            col.id === source.droppableId ? { ...col, tasks: sourceTasks } :
+            col.id === destination.droppableId ? { ...col, tasks: destinationTasks } : col
+          );
+  
+          setBoardState({ columns: newColumns });
+        }
       }
     }
   };
+  
 
   return (
     <div className={isDark ? 'bg-black h-screen' : ''}>
